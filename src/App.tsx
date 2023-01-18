7db692431e8a4d438ae94d9953da0a6edd1c2a4a
Ericson Moreira <moreira.ericson@gmail.com>
2023-01-18 11:13:32 -0300

    🚧 ajustando imposts

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
-import { useState } from "react";
-import atmLogo from "./assets/atm-logo.png";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <div>
-        <img src={atmLogo} alt="React logo" />
-      </div>
+      <img src="/atm-logo.png" alt="React logo" />
       <h1>ATM Card House</h1>
       <h2>Em breve!</h2>
     </div>
