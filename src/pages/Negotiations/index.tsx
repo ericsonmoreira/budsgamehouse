b1e6c0297397721b81184d052d63d4651db588a0
Ericson Moreira <moreira.ericson@gmail.com>
2023-03-04 13:07:09 -0300

    ultima atualização

--- src/pages/Negotiations/index.tsx
+++ src/pages/Negotiations/index.tsx
@@ -1,7 +1,33 @@
-import { Typography } from "@mui/material";
+import { Typography, Box, Tooltip, IconButton } from "@mui/material";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import { useState } from "react";
 
 const Negotiations: React.FC = () => {
-  return <Typography>Negotiations</Typography>;
+  const [addNegotiationDialogOpen, setAddNegotiationDialogOpen] =
+    useState(false);
+
+  return (
+    <>
+      <Box
+        sx={{
+          margin: 1,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <Typography variant="h4">Negociações</Typography>
+        <Tooltip title="Add">
+          <IconButton
+            color="secondary"
+            onClick={() => setAddNegotiationDialogOpen(true)}
+          >
+            <AddCircleIcon fontSize="large" />
+          </IconButton>
+        </Tooltip>
+      </Box>
+    </>
+  );
 };
 
 export default Negotiations;
