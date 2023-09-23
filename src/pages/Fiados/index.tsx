9eaa01498bb68874d73d476e9aac15194b3230fd
Ericson Moreira <moreira.ericson@gmail.com>
2023-09-23 11:04:56 -0300

    🔥 deletar fiado

--- src/pages/Fiados/index.tsx
+++ src/pages/Fiados/index.tsx
@@ -1,14 +1,54 @@
 import AddCircleIcon from '@mui/icons-material/AddCircle';
-import { Box, IconButton, Tooltip, Typography } from '@mui/material';
+import {
+  Backdrop,
+  Box,
+  Button,
+  CircularProgress,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+  IconButton,
+  Tooltip,
+  Typography,
+} from '@mui/material';
 import React, { useState } from 'react';
 import DataGridFiados from '../../components/datagrids/DataGridFiados';
 import AddFiadoDialog from '../../components/dialogs/fiados/AddFiadoDialog';
 import useFiados from '../../hooks/useFiados';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import deleteFiado from '../../resources/fiados/deleteFiado';
+import toast from 'react-hot-toast';
 
 const Fiados: React.FC = () => {
+  const queryClient = useQueryClient();
+
   const [addFiadoDialogOpen, setAddFiadoDialogOpen] = useState(false);
 
+  const [deleteFiadoDialogOpen, setDeleteFiadoDialogOpen] = useState(false);
+
+  const [fiadoToDeleteSelected, setFiadoToDeleteSelected] = useState<Fiado>({} as Fiado);
+
   const { data, isLoading } = useFiados();
+
+  const { mutate: deleteFiadoMutate, isLoading: deleteFiadoMutateIsloading } = useMutation({
+    mutationFn: async () => {
+      setDeleteFiadoDialogOpen(false);
+
+      await deleteFiado(fiadoToDeleteSelected.id);
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries(['useFiados']);
+
+      toast.success('Fiado removido com sucesso');
+    },
+  });
+
+  const handledelete = (fiadoToDelete: Fiado) => {
+    setFiadoToDeleteSelected(fiadoToDelete);
+    setDeleteFiadoDialogOpen(true);
+  };
 
   return (
     <>
@@ -34,7 +74,7 @@ const Fiados: React.FC = () => {
           rows={data?.map((fiado) => ({
             ...fiado,
             actions: {
-              handledelete: () => {},
+              handledelete: () => handledelete(fiado),
               handleUpdate: () => {},
             },
           }))}
@@ -48,6 +88,22 @@ const Fiados: React.FC = () => {
         setOpen={setAddFiadoDialogOpen}
         onClose={() => setAddFiadoDialogOpen(false)}
       />
+      <Dialog fullWidth maxWidth="md" open={deleteFiadoDialogOpen} onClose={() => setDeleteFiadoDialogOpen(false)}>
+        <DialogTitle>Remover Fiado</DialogTitle>
+        <DialogContent>
+          <DialogContentText>Deseja realmente remover esse Fiado?</DialogContentText>
+          <DialogContentText variant="overline">Essa ação não pode ser desfeita!</DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button variant="contained" color="error" onClick={() => setDeleteFiadoDialogOpen(false)}>
+            Cancelar
+          </Button>
+          <Button onClick={() => deleteFiadoMutate()}>Confirmar</Button>
+        </DialogActions>
+      </Dialog>
+      <Backdrop sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }} open={deleteFiadoMutateIsloading}>
+        <CircularProgress color="primary" />
+      </Backdrop>
     </>
   );
 };
