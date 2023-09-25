36ac4fd304de667d46ad3b6c6d31c2d5e5705e22
Ericson Moreira <moreira.ericson@gmail.com>
2023-09-25 11:52:33 -0300

    informações adicionais dos saldos

--- src/pages/Balances/index.tsx
+++ src/pages/Balances/index.tsx
@@ -1,9 +1,20 @@
 import AddCircleIcon from '@mui/icons-material/AddCircle';
-import { Box, IconButton, Tooltip, Typography } from '@mui/material';
-import React, { useState } from 'react';
+import PersonIcon from '@mui/icons-material/Person';
+import { Box, Chip, IconButton, Stack, Tooltip, Typography } from '@mui/material';
+import React, { useMemo, useState } from 'react';
 import DataGridBalances from '../../components/datagrids/DataGridBalances';
 import UpdateBalanceDialog from '../../components/dialogs/balances/UpdateBalanceDialog';
 import usePlayers from '../../hooks/usePlayers';
+import { formatterCurrencyBRL } from '../../utils/formatters';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
+
+type ContentCard = {
+  title: string;
+  tooltipTitle: string;
+  value: string | number;
+  color: 'success' | 'default' | 'primary' | 'secondary' | 'error' | 'info' | 'warning';
+  icon: React.ReactElement;
+};
 
 const Balances: React.FC = () => {
   const { players } = usePlayers();
@@ -11,6 +22,76 @@ const Balances: React.FC = () => {
   const [updateFiadoDialogOpen, setUpdateFiadoDialogOpen] = useState(false);
 
   const [playerToUpdate, setPlayerToUpdate] = useState<Player>({} as Player);
+
+  const totalPlayersNegativeBaleance = useMemo(() => {
+    if (players) {
+      return players.filter((player) => player.balance < 0).length;
+    }
+
+    return 0;
+  }, [players]);
+
+  const totalPlayersPositiveBalance = useMemo(() => {
+    if (players) {
+      return players.filter((player) => player.balance > 0).length;
+    }
+
+    return 0;
+  }, [players]);
+
+  const totalPositiveBaleances = useMemo(() => {
+    if (players) {
+      return players.filter((player) => player.balance > 0).reduce((acc, curr) => acc + curr.balance, 0);
+    }
+
+    return 0;
+  }, [players]);
+
+  const totalNegativeBaleances = useMemo(() => {
+    if (players) {
+      return players.filter((player) => player.balance < 0).reduce((acc, curr) => acc + curr.balance, 0);
+    }
+
+    return 0;
+  }, [players]);
+
+  const contentCards: ContentCard[] = [
+    {
+      title: 'Jogadores em Débito',
+      tooltipTitle: 'Total de jogadores com saldo negativo',
+      value: totalPlayersNegativeBaleance,
+      color: 'error',
+      icon: <PersonIcon />,
+    },
+    {
+      title: 'Jogadores com Saldo',
+      tooltipTitle: 'Total de jogadores com saldo positivo',
+      value: totalPlayersPositiveBalance,
+      color: 'success',
+      icon: <PersonIcon />,
+    },
+    {
+      title: 'Total de Fiados',
+      tooltipTitle: 'Somatório dos saldos negatvios dos jogadores',
+      value: formatterCurrencyBRL.format(totalNegativeBaleances),
+      color: 'error',
+      icon: <AttachMoneyIcon />,
+    },
+    {
+      title: 'Total de Saldos',
+      tooltipTitle: 'Somatório dos saldos positivos dos jogadores',
+      value: formatterCurrencyBRL.format(totalPositiveBaleances),
+      color: 'success',
+      icon: <AttachMoneyIcon />,
+    },
+    {
+      title: 'Balanço Geral',
+      tooltipTitle: 'Somatório dos saldos positivos dos jogadores',
+      value: formatterCurrencyBRL.format(totalPositiveBaleances + totalNegativeBaleances),
+      color: 'secondary',
+      icon: <AttachMoneyIcon />,
+    },
+  ];
 
   const handleUpdate = (player: Player) => {
     setPlayerToUpdate(player);
@@ -37,7 +118,16 @@ const Balances: React.FC = () => {
           </IconButton>
         </Tooltip>
       </Box>
-      <Box sx={{ margin: 1, height: 1 }}>
+      <Box mx={1}>
+        <Stack direction="row" spacing={1}>
+          {contentCards.map(({ title, tooltipTitle, value, color, icon }, index) => (
+            <Tooltip title={tooltipTitle} key={index}>
+              <Chip size="small" icon={icon} color={color} variant="outlined" label={`${title}: ${value}`} />
+            </Tooltip>
+          ))}
+        </Stack>
+      </Box>
+      <Box m={1} sx={{ height: 1 }}>
         <DataGridBalances
           rows={players?.map((player) => ({
             ...player,
