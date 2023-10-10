3fdc3de1f4aa703207e4f3983be49276aa684fe7
Ericson Moreira <moreira.ericson@gmail.com>
2023-10-10 08:36:31 -0300

    ⚡️ajustes nas telas de login e de recuperar senha

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,14 +1,16 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Button, CircularProgress, Paper, Stack, Typography } from '@mui/material';
+import { Button, Paper, Stack, Typography } from '@mui/material';
+import { useMutation } from '@tanstack/react-query';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { Navigate, useNavigate } from 'react-router-dom';
+import Page from '../../components/Page';
 import ControlledPasswordTextField from '../../components/textfields/ControlledPasswordTextField';
 import ControlledTextField from '../../components/textfields/ControlledTextField';
 import routesNames from '../../routes/routesNames';
 import { auth } from '../../services/firebaseConfig';
-import schema from './schema ';
 import verifyFirebaseErroCode from '../../services/verifyFirebaseErroCode';
+import schema from './schema ';
 
 type LoginFormData = {
   email: string;
@@ -24,12 +26,17 @@ const Login: React.FC = () => {
     resolver: yupResolver(schema),
   });
 
-  const onSubmit: SubmitHandler<LoginFormData> = async ({ email, password }) => {
-    try {
+  const { mutate: signInWithEmailAndPasswordMutation } = useMutation({
+    mutationFn: async ({ email, password }: LoginFormData) => {
       await signInWithEmailAndPassword(email, password);
-    } catch (e) {
-      console.log('erro');
-    }
+    },
+    onSuccess: () => {
+      navigate(routesNames.HOME);
+    },
+  });
+
+  const onSubmit: SubmitHandler<LoginFormData> = async ({ email, password }) => {
+    signInWithEmailAndPasswordMutation({ email, password });
   };
 
   if (user) {
@@ -37,54 +44,50 @@ const Login: React.FC = () => {
   }
 
   return (
-    <Paper
-      elevation={0}
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        p: 2,
-        width: '100%',
-        background: 'rgba(0, 0, 0, 0.25)',
-        backdropFilter: 'blur(5px)',
-      }}
-    >
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Stack
-          spacing={2}
-          sx={{
-            display: 'flex',
-            flex: 1,
-          }}
-        >
-          <Typography variant="h6">Login ATM</Typography>
-          <ControlledTextField name="email" control={control} variant="outlined" size="small" label="Email" />
-          <ControlledPasswordTextField
-            name="password"
-            control={control}
-            variant="outlined"
-            size="small"
-            label="Password"
-          />
-          {signError && (
-            <Typography sx={{ color: (theme) => theme.palette.error.dark }}>
-              {verifyFirebaseErroCode(signError.code)}
-            </Typography>
-          )}
-          <Button
-            disableElevation
-            type="submit"
-            variant="contained"
-            disabled={loading}
-            endIcon={loading && <CircularProgress size={12} color="info" sx={{ marginLeft: 2 }} />}
+    <Page loading={loading}>
+      <Paper
+        elevation={0}
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          p: 2,
+          width: '100%',
+          background: 'rgba(0, 0, 0, 0.25)',
+          backdropFilter: 'blur(5px)',
+        }}
+      >
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Stack
+            spacing={2}
+            sx={{
+              display: 'flex',
+              flex: 1,
+            }}
           >
-            Login
-          </Button>
-          <Button variant="text" onClick={() => navigate(routesNames.RECOVER_PASSWORD)}>
-            Redefinir senha
-          </Button>
-        </Stack>
-      </form>
-    </Paper>
+            <Typography variant="h6">Login ATM</Typography>
+            <ControlledTextField name="email" control={control} variant="outlined" size="small" label="Email" />
+            <ControlledPasswordTextField
+              name="password"
+              control={control}
+              variant="outlined"
+              size="small"
+              label="Password"
+            />
+            {signError && (
+              <Typography sx={{ color: (theme) => theme.palette.error.dark }}>
+                {verifyFirebaseErroCode(signError.code)}
+              </Typography>
+            )}
+            <Button disableElevation type="submit" variant="contained">
+              Login
+            </Button>
+            <Button variant="text" onClick={() => navigate(routesNames.RECOVER_PASSWORD)}>
+              Redefinir senha
+            </Button>
+          </Stack>
+        </form>
+      </Paper>
+    </Page>
   );
 };
 
--- src/pages/RecoverPassword/index.tsx
+++ src/pages/RecoverPassword/index.tsx
@@ -1,16 +1,18 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import { Box, Button, CircularProgress, Paper, Stack, Typography } from '@mui/material';
+import { useMutation } from '@tanstack/react-query';
+import { AuthError } from 'firebase/auth';
 import { useSendPasswordResetEmail } from 'react-firebase-hooks/auth';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import { toast } from 'react-hot-toast';
 import { useNavigate } from 'react-router-dom';
+import Page from '../../components/Page';
 import ControlledTextField from '../../components/textfields/ControlledTextField';
 import routesNames from '../../routes/routesNames';
 import { auth } from '../../services/firebaseConfig';
-import schema from './schema ';
-import { AuthError } from 'firebase/auth';
 import verifyFirebaseErroCode from '../../services/verifyFirebaseErroCode';
+import schema from './schema ';
 
 type RecoverPasswordData = {
   email: string;
@@ -25,72 +27,81 @@ const RecoverPassword: React.FC = () => {
 
   const [sendPasswordResetEmail, sending, error] = useSendPasswordResetEmail(auth);
 
-  const onSubmit: SubmitHandler<RecoverPasswordData> = async ({ email }) => {
-    try {
-      const success = await sendPasswordResetEmail(email);
+  const { mutate: sendPasswordResetEmailMutation, isLoading } = useMutation({
+    mutationFn: async (email: string) => {
+      await sendPasswordResetEmail(email);
+    },
+    onSuccess: () => {
+      toast.success('Email enviado com sucesso');
+    },
+    onError: (e: Error) => {
+      toast.error(e.message);
+    },
+  });
 
-      if (success) toast.success('Email enviado com sucessor');
-    } catch (e) {
-      console.log('erro');
-    }
+  const onSubmit: SubmitHandler<RecoverPasswordData> = async ({ email }) => {
+    sendPasswordResetEmailMutation(email);
   };
+
   return (
-    <Paper
-      elevation={0}
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        p: 2,
-        width: '100%',
-        background: 'rgba(255, 255, 255, 0.75)',
-        backdropFilter: 'blur(5px)',
-      }}
-    >
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Stack
-          spacing={2}
-          sx={{
-            display: 'flex',
-            flex: 1,
-          }}
-        >
-          <Typography variant="h6">Redefinir senha</Typography>
-          <ControlledTextField name="email" control={control} variant="outlined" size="small" label="Email" />
-          {error && (
-            <Typography sx={{ color: (theme) => theme.palette.error.dark }}>
-              {verifyFirebaseErroCode((error as AuthError).code)}
-            </Typography>
-          )}
-          <Box
+    <Page loading={isLoading}>
+      <Paper
+        elevation={0}
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          p: 2,
+          width: '100%',
+          background: 'rgba(0, 0, 0, 0.25)',
+          backdropFilter: 'blur(5px)',
+        }}
+      >
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Stack
+            spacing={2}
             sx={{
               display: 'flex',
-              width: '100%',
-              justifyContent: 'space-between',
+              flex: 1,
             }}
           >
-            <Button
-              disableElevation
-              type="submit"
-              variant="text"
-              disabled={sending}
-              startIcon={<ArrowBackIcon />}
-              onClick={() => navigate(routesNames.LOGIN)}
+            <Typography variant="h6">Redefinir senha</Typography>
+            <ControlledTextField name="email" control={control} variant="outlined" size="small" label="Email" />
+            {error && (
+              <Typography sx={{ color: (theme) => theme.palette.error.dark }}>
+                {verifyFirebaseErroCode((error as AuthError).code)}
+              </Typography>
+            )}
+            <Box
+              sx={{
+                display: 'flex',
+                width: '100%',
+                justifyContent: 'space-between',
+              }}
             >
-              Voltar para Tela de Login
-            </Button>
-            <Button
-              disableElevation
-              type="submit"
-              variant="contained"
-              disabled={sending || !!error} // necessário travar o botão quando acontece um errro
-              endIcon={sending && <CircularProgress size={12} color="info" sx={{ marginLeft: 2 }} />}
-            >
-              Enviar Email
-            </Button>
-          </Box>
-        </Stack>
-      </form>
-    </Paper>
+              <Button
+                disableElevation
+                type="submit"
+                variant="text"
+                disabled={sending}
+                startIcon={<ArrowBackIcon />}
+                onClick={() => navigate(routesNames.LOGIN)}
+              >
+                Voltar
+              </Button>
+              <Button
+                disableElevation
+                type="submit"
+                variant="contained"
+                disabled={sending || !!error} // necessário travar o botão quando acontece um errro
+                endIcon={sending && <CircularProgress size={12} color="info" sx={{ marginLeft: 2 }} />}
+              >
+                Enviar
+              </Button>
+            </Box>
+          </Stack>
+        </form>
+      </Paper>
+    </Page>
   );
 };
 
