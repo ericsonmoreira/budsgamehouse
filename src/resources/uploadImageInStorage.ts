91c4796b1d34a4b1d8f3c19d37ad4e0d81d45769
Ericson Moreira <moreira.ericson@gmail.com>
2023-10-13 12:35:18 -0300

    Adicionando preprocessamento de envio de imagens

--- src/resources/uploadImageInStorage.ts
+++ src/resources/uploadImageInStorage.ts
@@ -1,10 +1,14 @@
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import { storage } from '../services/firebaseConfig';
 
+import reduce from 'image-blob-reduce';
+
 const uploadImageInStorage = async (image: File) => {
+  const reduceImage = await reduce().toBlob(image, { max: 200 });
+
   const storageRef = ref(storage, `images/${image.name + Date.now()}`);
 
-  await uploadBytes(storageRef, image);
+  await uploadBytes(storageRef, reduceImage);
 
   return await getDownloadURL(storageRef);
 };
