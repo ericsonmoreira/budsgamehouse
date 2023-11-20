b97e274aaac3d01f4bc0f34b299e7c3023238f1e
Ericson Moreira <moreira.ericson@gmail.com>
2023-11-20 17:04:47 -0300

    ajuste de theme

---
+++ src/hooks/useLastTwelveMonths.tsx
@@ -0,0 +1,24 @@
+import { format, subMonths } from 'date-fns';
+import { useMemo } from 'react';
+
+function useLastTwelveMonths(quant: number) {
+  // últimos 12 meses
+  const lastTwelveMonths = useMemo(() => {
+    const now = Date.now();
+
+    const months: string[] = []; // MM/yyyy
+
+    for (let i = 0; i < quant; i++) {
+      const data = subMonths(now, i);
+
+      const mesAno = format(data, 'MM/yyyy');
+      months.push(mesAno);
+    }
+
+    return months;
+  }, [quant]);
+
+  return lastTwelveMonths;
+}
+
+export default useLastTwelveMonths;
--- src/hooks/useSales.tsx
+++ src/hooks/useSales.tsx
@@ -1,19 +1,7 @@
-import { useQuery } from '@tanstack/react-query';
-import { collection, getDocs } from 'firebase/firestore';
-import { firestore } from '../services/firebaseConfig';
-
-const path = 'sales';
-
-const queryKey = 'useSales';
+import useReactQuery from './useReactQuery';
 
 function useSales() {
-  const salesCollectionRef = collection(firestore, path);
-
-  return useQuery([queryKey], async () => {
-    const { docs } = await getDocs(salesCollectionRef);
-
-    return [...docs.map((doc) => ({ id: doc.id, ...doc.data() } as Sale))];
-  });
+  return useReactQuery<Product>({ path: 'sales', key: useSales.name });
 }
 
 export default useSales;
