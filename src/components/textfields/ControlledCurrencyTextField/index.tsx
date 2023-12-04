484478505baf4c3ec2b3e696b76cbfd8b7875554
Ericson Moreira <moreira.ericson@gmail.com>
2023-12-04 17:16:57 -0300

    Ajustes no component de valores

--- src/components/textfields/ControlledCurrencyTextField/index.tsx
+++ src/components/textfields/ControlledCurrencyTextField/index.tsx
@@ -1,11 +1,13 @@
 import { InputAdornment, TextField, TextFieldProps } from '@mui/material';
 import { FieldPath, FieldValues, UseControllerProps, useController } from 'react-hook-form';
-import { NumericFormat, NumericFormatProps } from 'react-number-format';
+import { NumberFormatBase, NumberFormatBaseProps } from 'react-number-format';
+import { FormatInputValueFunction } from 'react-number-format/types/types';
+import { formatterDecimal } from '../../../utils/formatters';
 
 type ControlledCurrencyTextFieldProps<
   TextFieldValues extends FieldValues,
   TextFieldName extends FieldPath<TextFieldValues>
-> = UseControllerProps<TextFieldValues, TextFieldName> & NumericFormatProps<TextFieldProps>;
+> = UseControllerProps<TextFieldValues, TextFieldName> & NumberFormatBaseProps<TextFieldProps>;
 
 const ControlledCurrencyTextField = <
   TextFieldValues extends FieldValues,
@@ -20,20 +22,23 @@ const ControlledCurrencyTextField = <
     fieldState: { error },
   } = useController({ control, name });
 
+  const format: FormatInputValueFunction = (numString) => {
+    const numSanitized = numString.replace(/[^0-9.]/g, '');
+
+    return formatterDecimal.format(Number(numSanitized || 0) / 100);
+  };
+
   return (
-    <NumericFormat
+    <NumberFormatBase
       {...rest}
       {...fieldRest}
+      value={null}
       getInputRef={ref}
       error={!!error}
       helperText={error?.message}
       customInput={TextField}
-      allowLeadingZeros
-      fixedDecimalScale
-      thousandSeparator="."
-      decimalSeparator=","
-      decimalScale={2}
-      onValueChange={(values) => onChange(values.floatValue ?? 0)}
+      format={format}
+      onValueChange={(values) => onChange((values.floatValue || 0) / 100)}
       InputProps={{
         startAdornment: <InputAdornment position="start">R$</InputAdornment>,
       }}
