434fd665ab8ef79646d12af794140e8cec6e707d
Ericson Moreira <moreira.ericson@gmail.com>
2024-11-17 06:10:27 -0300

    melhoria na verificação da data da comanda

--- src/components/CommandCard/index.tsx
+++ src/components/CommandCard/index.tsx
@@ -1,8 +1,9 @@
 import { Box, ButtonBase, Paper, Typography } from '@mui/material';
-import { format } from 'date-fns';
+import { differenceInHours, format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import React, { useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { WarningIcon } from '../../icons';
 import routesNames from '../../routes/routesNames';
 import CommandTitleName from '../CommandTitleName';
 import TypographyBalance from '../TypographyBalance';
@@ -18,20 +19,38 @@ const CommandCard: React.FC<CommandCardProps> = ({ data }) => {
     return data.products.reduce((acc, curr) => acc + curr.price * curr.amount, 0);
   }, [data]);
 
+  // verifica se a comanda está aberta a mais de 24 horas
+  const checkDelay = (command: Command): boolean => {
+    const now = new Date();
+
+    const createdAtDate = new Date(Number(command.createdAt.toString()));
+
+    return command.status === 'open' && differenceInHours(now, createdAtDate) > 24;
+  };
+
   return (
     <ButtonBase
       TouchRippleProps={{ center: false }}
       sx={{ width: '100%' }}
       onClick={() => navigate(routesNames.VIEW_COMMAND.replace(':id', data.id))}
     >
-      <Paper sx={{ width: '100%' }}>
+      <Paper
+        sx={({ palette }) => ({
+          width: '100%',
+          borderColor: checkDelay(data) ? palette.warning.main : palette.divider,
+        })}
+        variant="outlined"
+      >
         <Box p={2}>
           <CommandTitleName command={data} />
           <Typography variant="h6">Cliente: {data.displayName || 'Não Informado'}</Typography>
           <TypographyBalance variant="h4" balance={total} />
-          <Typography variant="caption" color="GrayText">
-            Aberto em {format(data.createdAt, 'PPPp', { locale: ptBR })}
-          </Typography>
+          <Box gap={1} display="flex" alignItems="center" justifyContent="center">
+            <Typography variant="button" color="GrayText">
+              Aberto em {format(data.createdAt, 'PPPp', { locale: ptBR })}
+            </Typography>
+            {checkDelay(data) && <WarningIcon fontSize="large" color="warning" />}
+          </Box>
         </Box>
       </Paper>
     </ButtonBase>
