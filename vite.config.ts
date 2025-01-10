549a9c6374c14c13574d3b9a766d89ecafeab5f9
Ericson Moreira <moreira.ericson@gmail.com>
2025-01-10 13:32:39 -0300

    configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { qrcode } from "vite-plugin-qrcode";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [qrcode()],
+  plugins: [react()],
   test: {
     include: ["**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
   },
